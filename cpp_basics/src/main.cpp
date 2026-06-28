bb47253cee5da976109abc71c5dc3d0f4de4e24f
Zzq1 <1085200459@qq.com>
2026-06-28 22:27:28 +0800

    Read link lengths from command line

--- cpp_basics/src/main.cpp
+++ cpp_basics/src/main.cpp
@@ -13,27 +13,29 @@
 // C++ 程序的入口函数。
 // 程序运行时，会从 main() 里面的第一行开始执行。
 int main(int argc, char* argv[]) {
-    
-    // 程序需要两个输入参数：theta1_deg和theta2_deg，分别表示两个关节的角度，单位是度。
-    if (argc != 3) {
+    // 程序需要 4 个输入参数：l1, l2, theta1_deg, theta2_deg。
+    // 加上程序名本身，argc 应该等于 5。
+    if (argc != 5) {
         // 如果参数数量不对，就打印提示信息，并返回错误码 1。
-        std::cout << "Usage: ./build/forward_kinematics theta1_deg theta2_deg\n";
+        std::cout << "Usage: ./build/forward_kinematics l1 l2 theta1_deg theta2_deg\n";
         return 1;
     }
 
     // 第一段连杆长度，单位是m。
-    const double l1 = 0.30;
+    // argv[1] 是命令行输入的第1个参数，类型是 char*，需要用 std::stod 转换为 double。
+    const double l1 = std::stod(argv[1]);
 
     // 第二段连杆长度，单位是m。
-    const double l2 = 0.20;
+    // argv[2] 是命令行输入的第2个参数，类型是 char*，需要用 std::stod 转换为 double。
+    const double l2 = std::stod(argv[2]);
 
     // 第一个关节的角度，单位是度。
-    // argv[1] 是命令行输入的第一个参数，类型是 char*，需要用 std::stod 转换为 double。
-    const double theta1_deg = std::stod(argv[1]);
+    // argv[3] 是命令行输入的第3个参数，类型是 char*，需要用 std::stod 转换为 double。
+    const double theta1_deg = std::stod(argv[3]);
 
     // 第二个关节的角度，单位是度。
-    // argv[2] 是命令行输入的第二个参数，类型是 char*，需要用 std::stod 转换为 double。
-    const double theta2_deg = std::stod(argv[2]);
+    // argv[4] 是命令行输入的第4个参数，类型是 char*，需要用 std::stod 转换为 double。
+    const double theta2_deg = std::stod(argv[4]);
 
     // 调用2R正运动学函数，计算末端执行器的坐标。
     const Eigen::Vector2d position = compute_2r_forward_kinematics(
