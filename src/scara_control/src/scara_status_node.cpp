066d41d7ddf9cf215f7a15372ffc3e4a7ca7f57e
Zzq1 <1085200459@qq.com>
2026-07-03 23:40:39 +0800

    Publish SCARA joint states

--- src/scara_control/src/scara_status_node.cpp
+++ src/scara_control/src/scara_status_node.cpp
@@ -1,5 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
-
+#include "sensor_msgs/msg/joint_state.hpp"
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -11,19 +11,40 @@ class ScaraStatusNode : public rclcpp::Node
 public:
     ScaraStatusNode()
     : Node("scara_status_node")
-    {
+    {   
+        joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>(
+            "/joint_states",
+            10
+        );
+
         timer_ = this->create_wall_timer(
             1s,
-            std::bind(&ScaraStatusNode::print_status, this));
+            std::bind(&ScaraStatusNode::publish_joint_state, this)
+        );
     }
-
+ 
 private:
     void print_status()
     {
         RCLCPP_INFO(this->get_logger(), "SCARA control node is alive.");
     }
 
+    double joint1_position_ = 0.0;
+
+    void publish_joint_state()
+    {
+        sensor_msgs::msg::JointState msg;
+        msg.header.stamp = this->now();
+        msg.name = {"joint1", "joint2", "joint_z", "joint_tool"};
+        joint1_position_ += 0.1;
+        msg.position = {joint1_position_, 0.0, 0.0, 0.0};
+
+        joint_state_publisher_->publish(msg);
+        RCLCPP_INFO(this->get_logger(), "Publish SCARA joint state.");
+    }
+
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher_;
 };
 
 int main(int argc, char ** argv)
